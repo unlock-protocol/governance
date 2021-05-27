f355c0a1df8cf932b477198e44ee8432add4e797
Clement Renaud <clement@clementrenaud.com>
2021-05-27 12:55:06 +0200

    basic deployment task

--- README.md
+++ README.md
@@ -4,4 +4,8 @@ Governance tools for Unlock Protocol
 
 Read the [proposal](https://www.notion.so/unlockprotocol/Governance-DAO-c007ec3efab942b99d5903bb87e58b48)
 
+## Deploy the guardian
 
+```
+npx hardhat deploy --token 0xAddressToReceivetokens --timelock 0xAddressTimeLockAdmin --guardian 0xAddressGovernorAlphaAdmin --network rinkeby
+```
--- hardhat.config.js
+++ hardhat.config.js
@@ -10,6 +10,22 @@ task("accounts", "Prints the list of accounts", async () => {
   }
 });
 
+task("deploy", "Deploys a COMPound style governance system")
+  .addParam("token", "The address to receive the initial supply")
+  .addParam("timelock", "The timelock administrator")
+  .addParam("guardian", "The governor guardian")
+  .setAction(async taskArgs => {
+
+    const { deploy } = require("./scripts/deploy");
+
+    await deploy({
+      tokenRecipient: taskArgs.token,
+      timeLockAdmin: taskArgs.timelock,
+      guardian: taskArgs.guardian
+    });
+
+  })
+
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
@@ -17,12 +33,12 @@ task("accounts", "Prints the list of accounts", async () => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  networks: {
-    rinkeby: {
-      url: "https://rinkeby.infura.io/v3/<<Your infura Key>>",
-      accounts: ["PrivateKey1", "PrivateKey2", "PrivateKey3"]
-    }
-  },
+  // networks: {
+  //   rinkeby: {
+  //     url: "https://rinkeby.infura.io/v3/<<Your infura Key>>",
+  //     accounts: ["PrivateKey1", "PrivateKey2", "PrivateKey3"]
+  //   }
+  // },
   solidity: "0.5.16",
 };
 
